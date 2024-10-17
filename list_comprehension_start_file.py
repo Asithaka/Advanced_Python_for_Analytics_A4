09d2b2dc935c49376cf222c61ae1fddd14c60566
Asitha Mudiyanselage <asitha_mudiyanselage@baylor.edu>
2024-10-17 15:13:07 -0500

    Started agmt4

--- list_comprehension_start_file.py
+++ list_comprehension_start_file.py
@@ -30,7 +30,101 @@ for i in original_list:
 
 #The filter part answers the question if the item should be transformed.
 
+old_list = [1,2,3,4,5]
+new_list = []
 
+for i in old_list:
+    if i >3:
+        j=i*i
+        new_list.append(j)
+print(new_list)
+
+
+# [expression iteration condition]
+new_list = [i*i for i in old_list if i>3]
+print(new_list)
+
+#1) creating a simple list of 10 numbers using  Range()
+
+x = [i for i in range(10)]
+print(x)
+
+#2) creating  a list that evaluates an expression
+
+squares = [x**2 for x in range(10)]
+print(squares)
+
+
+#3) creating a list from another list
+
+list1 = [3,4,5]
+multiplied = [item *3 for item in list1]
+print(multiplied)
+
+#4) using list comprehension for string manipulation
+
+# printout word with first letter is being upper case
+
+listOfWords = ["The", "Force", "will","be","with","you","Always"]
+
+word_type =[ item[0] for item in listOfWords if item[0] == item[0].upper()]
+
+print(word_type)
+
+#5) Let's show how easy you can convert lower case ? upper case letters.
+
+output = [x.lower() for x in ["A", "B","C"]]
+print(output)
+
+#6) Creating a list based on a condition
+
+# create a list of squre of all even numbers between 1 and 10 
+
+output = [x*x for x in range(1,11) if x % 2 == 0]
+print(output)
+
+#7) Extracting numbers only from a string and putting it in a list
+
+string = "Hello 12345 World"
+numbers = [ int(x)for x in string if x.isdigit() ]
+print(numbers)
+
+letters = [ x for x in string if x.isalpha()]
+print(letters)
+
+words = [x for x in string.split(" ") if x.isalpha()]
+print(words)
+
+#8) 
+'''
+In this example, we can see how to get 
+specific lines out from a text file
+
+'''
+
+infile = open( 'test.txt','r')
+
+result = [ i.rstrip("\n") for i in infile if "line3" in i]
+
+print(result)
+
+#9) Using function in list comprehension
+
+def double(x):
+    return x*2
+
+result = [ double(x) for x in range(10)]
+print(result)
+
+#10) adding an IF condition to the above
+
+result = [ double(x) for x in range(10) if x % 2 ==1]
+print(result)
+
+#11) you can add more argument (using multiple iterations and lists):
+
+output = [x+y for x in [10,50] for y in [20,40] if x+y > 70]
+print(output)
 
 ## Exercise ##
 
@@ -39,7 +133,8 @@ for i in original_list:
 
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 
-
+newlist = [int(x) for x in numbers if x > 0]
+print(newlist)
 
 
 ## 2 create a list of integers which specify the length of each word in
@@ -48,7 +143,8 @@ numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 sentence = "the quick brown fox jumps over the lazy dog"
 words = sentence.split()
 
-
+newlist = [int(len(x)) for x in words if x != 'the']
+print(newlist)
 
 ## Given dictionary is consisted of vehicles and their weights in kilograms. 
 ## Contruct a list of the names of vehicles with weight below 5000 kilograms. 
@@ -57,12 +153,14 @@ words = sentence.split()
 dict={"Sedan": 1500, "SUV": 2000, "Pickup": 2500, "Minivan": 1600, "Van": 2400, 
 "Semi": 13600, "Bicycle": 7, "Motorcycle": 110}
 
-
+newlist = [x.upper() for x,y in dict.items()  if y < 5000]
+print(newlist)
 
 
 ## Find all the numbers from 1 to 1000 that have a 4 in them
 
-
+newlist = [x for x in range(1,1001) if '4' in str(x)]
+print(newlist)
 
 ## count how many times the word 'the' appears in the text file - 'sometext.txt'
 
@@ -70,8 +168,7 @@ dict={"Sedan": 1500, "SUV": 2000, "Pickup": 2500, "Minivan": 1600, "Van": 2400,
 
 ## Extract the numbers from the following phrase ##
 
-phrase = 'In 1984 there were 13 instances of a protest with over 1000 people attending. On average there were 15 reported injuries at each " +
-"event, with about 3 or 4 that were classifled as serious per event.'
+phrase = 'In 1984 there were 13 instances of a protest with over 1000 people attending. On average there were 15 reported injuries at each event, with about 3 or 4 that were classifled as serious per event.'
 
 
 
